6d04acf0ae8e70590d7a2a82f6705b1cf6442148
Ian <ianpang126@gmail.com>
2024-10-26 22:15:04 -0400

    update ui

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -13,8 +13,8 @@ export default function Page() {
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Book Review Finder.</strong> This project scans
-            books from your bookshelf and looks up the ratings for you.
+            <strong>Welcome to ShelfMate.</strong> The easy way to access your
+            bookshelf.
           </p>
           <Link
             href="/dashboard"
@@ -26,7 +26,7 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
           <Image
-            src="/hero-desktop.png"
+            src="/hero-desktop.jpg"
             width={1000}
             height={760}
             className="hidden md:block"
